6b2acd87f88fc71b1fa18375b76f398832df42b0
Juan Souza <juan.sousa@dcomp.ufs.br>
2019-09-16 21:46:15 -0300

    Configuring urls

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import views
+from . import IndexTemplateView, FuncionarioListView, FuncionarioUpdateView,\
+    FuncionarioDeleteView, FuncionarioCreateView
 
 
 app_name = 'website'
@@ -7,5 +8,21 @@ app_name = 'website'
 # urlpatterns contains the URL routing list
 urlpatterns = [
     # GET /
-    path('', views.index, name='index'),
+    path('', IndexTemplateView(), name='index'),
+
+    # GET /funcionarios
+    path('funcionarios/', FuncionarioListView.as_view(),
+         name='lista_funcionarios'),
+
+    # GET/POST /funcionario/{pk}
+    path('funcionario/<pk>', FuncionarioUpdateView.as_view(),
+         name='atualiza_funcionario'),
+
+    # GET/POST /funcionario/excluir/{pk}
+    path('funcionario/excluir/<pk>', FuncionarioDeleteView.as_view(),
+         name='deleta_funcionario'),
+
+    # GET /funcionario/cadastrar
+    path('funcionario/cadastrar/', FuncionarioCreateView.as_view(),
+         name='cadastra_funcionario')
 ]
